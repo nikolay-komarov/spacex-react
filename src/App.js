619f4a91b347d785b0ed1f3039dae1f68f8d34af
nikolay-komarov <kny.komarov@gmail.com>
2020-10-14 17:24:56 +0300

    init + добавит файлы компонентов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Features from './components/features/features';
+import Footer from './components/footer/footer';
+// import Calendar from './components/calendar/calendar';
+// import Details from './components/details/details';
+
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Features />
+
+      {/* <Calendar /> */}
+      {/* <Details /> */}
+
+      <Footer />
+    </>
   );
 }
 
